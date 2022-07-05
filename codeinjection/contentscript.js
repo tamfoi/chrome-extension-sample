245bf8e13ab85e8efa8c3d5dd8274bf1c4c41d66
tamfoi <tamfoi@users.noreply.github.com>
2022-07-05 21:29:39 +0900

    codeinjectionのサンプルをv3に対応

--- codeinjection/contentscript.js
+++ codeinjection/contentscript.js
@@ -1,36 +1,14 @@
 console.log("contentscript");
 
 /**
- * インラインスクリプトとしてJavaScriptを注入する
- * @param {String} scriptText 注入するJavaScript
- */
-const injectScriptText = (scriptText) => {
-  const body = document.getElementsByTagName("body")[0];
-  const script = document.createElement("script");
-  script.innerHTML = scriptText;
-  body.appendChild(script);
-};
-
-/**
  * JavaScriptファイルを注入する
  * @param {String} filePath 注入するファイルのパス
  */
 const injectScriptFile = (filePath) => {
   const body = document.getElementsByTagName("body")[0];
   const script = document.createElement("script");
-  script.setAttribute("src", chrome.extension.getURL(filePath));
+  script.setAttribute("src", chrome.runtime.getURL(filePath));
   body.appendChild(script);
-};
-
-/**
- * styleタグでCSSを注入する
- * @param {String} styleText 注入するCSS
- */
-const injectStyleText = (styleText) => {
-  const head = document.getElementsByTagName("head")[0];
-  const style = document.createElement("style");
-  style.innerHTML = styleText;
-  head.appendChild(style);
 };
 
 /**
@@ -41,11 +19,9 @@ const injectStyleFile = (filePath) => {
   const head = document.getElementsByTagName("head")[0];
   const link = document.createElement("link");
   link.setAttribute("rel", "stylesheet");
-  link.setAttribute("href", chrome.extension.getURL(filePath));
+  link.setAttribute("href", chrome.runtime.getURL(filePath));
   head.appendChild(link);
 };
 
-injectScriptText("console.log('injectScriptText');");
 injectScriptFile("injectscript.js");
-injectStyleText("body{background-color: #efefef;}");
 injectStyleFile("injectstyle.css");
